fbd35c29f776574eaaa786e3798588405d41a11e
Fernando <fyonfa@gmail.com>
2020-11-03 12:05:03 +0000

    you are doing great

--- src/015-ninja-Level-9-concurrency/exercise-2/main.go
+++ src/015-ninja-Level-9-concurrency/exercise-2/main.go
@@ -2,30 +2,41 @@ package main
 
 import "fmt"
 
+//create a type person struct
+type person struct {
+	Name     string
+	Lastname string
+	Age      int
+}
+
+//attach a method speak to type person using a pointer receiver
+func (p *person) speak() {
+	fmt.Println("hola, this is my full name:", p.Name, p.Lastname, ".", "And, my age:", p.Age)
+	fmt.Printf("hola, this is my full name: %s %s, and my age %d", p.Name, p.Lastname, p.Age)
+	fmt.Println()
+}
+
+//create a type human interface
+//to implicitly implement the interface, a human must have the speak method
+type human interface {
+	speak()
+}
+
+func saySomething(h human) {
+
+	h.speak()
+
+}
+
 func main() {
-	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	n := foo(ii...)
-	fmt.Println(n)
 
-	ii2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	n2 := bar(ii2)
-	fmt.Println(n2)
-}
-
-func foo(xi ...int) int { //takes in xi as variadict parameter, return int
-	total := 0
-	for _, v := range xi {
-		total += v
+	p1 := person{
+		Name:     "Fernando",
+		Lastname: "Yonfa",
+		Age:      34,
 	}
-	return total
 
-}
-
-func bar(xi []int) int { //takes in xi as variadict parameter, return int
-	total := 0
-	for _, v := range xi {
-		total += v
-	}
-	return total
+	saySomething(&p1) //work with this
+	p1.speak()        //also with this :)
 
 }
